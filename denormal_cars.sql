89c77a89d6e74e12ddf7aa69535922951d11a59c
Pamela Yang <ypyang237@gmail.com>
2016-04-18 09:40:04 -1000

    finishes query lists for denormal.sql

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -3,3 +3,21 @@ CREATE ROLE denormal_user;
 DROP DATABASE IF EXISTS denormal_cars;
 CREATE DATABASE denormal_cars OWNER denormal_user;
 \c denormal_cars;
+
+\i scripts/denormal_data.sql;
+-- \dS;
+
+SELECT DISTINCT make_title
+FROM car_models;
+
+SELECT DISTINCT model_title
+FROM car_models
+WHERE make_code = 'VOLKS';
+
+SELECT make_code, model_title, year
+FROM car_models
+WHERE make_code = 'LAM';
+
+SELECT *
+FROM car_models
+WHERE year BETWEEN 2010 AND 2015;
